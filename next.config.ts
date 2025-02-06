ef692898f1316e0f89a3af7c76fe1352391fdbdb
Ahmad Moussa <gorillasuncontact@gmail.com>
2025-02-06 14:17:52 +0100

    embed branch test

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,20 @@ const nextConfig: NextConfig = {
   mdxRemark: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/embed/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
